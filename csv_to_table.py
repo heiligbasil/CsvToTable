083f92592358de12efc5838643b2eb8fd8fb2bbb
heiligbasil <basilief@yahoo.com>
2020-06-18 15:21:37 -0700

    Add function to calculate column length

--- csv_to_table.py
+++ csv_to_table.py
@@ -1,7 +1,20 @@
 import sys
 
 
+def calculate_column_count(items):
+    """Calculate the number of columns based on the length of the longest item in the list"""
+    global column_total
+    longest_length = len(max(items, key=len))
+    if longest_length < 15:
+        column_total = 5
+    elif longest_length < 25:
+        column_total = 4
+    else:
+        column_total = 3
+
+
 def table_print(items):
+    """Print the items to screen into the format of a pseudo table"""
     print()
     column = 1
     for item in items:
@@ -9,10 +22,11 @@ def table_print(items):
         column += 1
         if column > column_total:
             print()
-            column = 0
+            column = 1
 
 
 def table_html(items):
+    """Output the items to screen into a format useful in a HTML table"""
     print()
     print("<tr>")
     column = 1
@@ -24,13 +38,16 @@ def table_html(items):
         if column > column_total:
             print("</tr>")
             print("<tr>")
-            column = 0
+            column = 1
     print("</tr>")
 
 
+# Read in the contents of the .CSV file and split it into a list
 with open(sys.path[0] + '\\contents.csv') as f:
     split_items = f.read().split(sep=',')
 
-column_total = 4
+column_total = 1
+
+calculate_column_count(split_items)
 table_print(split_items)
 table_html(split_items)
